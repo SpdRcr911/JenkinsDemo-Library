3bd93529f1452589a85a9dfcd07e23bb6ef6340a
Rafael Delgado <SpdRcr911@users.noreply.github.com>
2020-12-15 08:47:51 -0800

    Update auditTools2.groovy

--- vars/auditTools2.groovy
+++ vars/auditTools2.groovy
@@ -2,8 +2,10 @@ def call(Map config) {
     node {
       echo ${config.message}
       sh '''
-        git version
-        docker version
+        git --version
+        docker --version
+        node --version
+        npm --version
         dotnet --list-sdks
         dotnet --list-runtimes
       '''
