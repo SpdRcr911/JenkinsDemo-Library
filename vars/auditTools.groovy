6680879814cedf9e86264ef8beda27763de30152
Rafael Delgado <SpdRcr911@users.noreply.github.com>
2020-12-15 08:47:34 -0800

    Update auditTools call

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -1,8 +1,11 @@
 def call() {
     node {
       sh '''
-        git version
-        docker version
+        git --version
+        docker --version
+        node --version
+        npm --version
+        newman --version
         dotnet --list-sdks
         dotnet --list-runtimes
       '''
